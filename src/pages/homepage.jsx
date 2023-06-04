b40ee5d66f139e1bbd3a7aae7f7088df1e72c91c
muhamadilhamh <ilham.habib190600@gmail.com>
2023-06-04 21:59:12 +0700

    fix router at content homepage to galery

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,12 +3,15 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Content from "../components/Content";
 import Carousel from "../components/Carousel";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <>
       <Hero />
-      <Content />
+      <Link to="/galeries">
+        <Content />
+      </Link>
       <Carousel />
     </>
   );
