d1a67fbcbeceae6613bd6f2953b574a3ac92f727
muhamadilhamh <ilham.habib190600@gmail.com>
2023-06-04 21:37:17 +0700

    feat modal at galery page

--- src/components/ListGaleries.jsx
+++ src/components/ListGaleries.jsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Modal from "./Modal";
 
 function ListGaleries(props) {
-  const data = props?.data;
-  const isLoading = props?.isLoading;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    console.log("masukk sini");
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    console.log("masuk sini");
+    setIsModalOpen(false);
+  }
+
+  console.log(isModalOpen);
+
   return (
     <div className="relative">
-      <img
-        className="h-full w-full object-cover"
-        src={props?.url}
-        alt={props?.title}
-      />
-      <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
-        <p className="left-4 bottom-4 text-2xl text-white absolute font-bold">
-          {props?.title}
-        </p>
-        <p className="left-2 top-4 text-2xl bg-transparent text-slate-50 -rotate-12  absolute font-bold">
-          {props?.category}
-        </p>
-      </div>
+      <Link to="#" onClick={openModal}>
+        <img
+          className="h-full w-full object-cover"
+          src={props?.url}
+          alt={props?.title}
+        />
+
+        <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+          <p className="left-4 bottom-4 text-lg text-white absolute font-bold">
+            {props?.title}
+          </p>
+          <p className="left-2 top-4 text-2xl bg-transparent text-slate-50 -rotate-12 absolute font-bold">
+            {props?.category}
+          </p>
+        </div>
+      </Link>
+
+      {isModalOpen && <Modal data={props} onClick={closeModal} />}
     </div>
   );
 }
